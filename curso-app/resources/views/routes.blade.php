2b8ce87580c93cd60232ec61111098833f90863e
Felipe1501 <felipebsantos1501@gmail.com>
2023-03-02 00:22:28 -0300

    testando routes e views em lavarel php

---
+++ curso-app/resources/views/routes.blade.php
@@ -0,0 +1,9 @@
+
+<style>
+    body{
+        background-color: #7bc8ce
+    }
+</style>
+<h2>testando routes em lavarel!!!</h2>
+<a href="/">voltar para o início</a><br>
+<a href="/test">teste 1</a>
---
+++ curso-app/resources/views/test.blade.php
@@ -0,0 +1,2 @@
+<h1>Só testando fiiiiii, testando rotas e views</h1>
+<a href="/">voltar</a>
