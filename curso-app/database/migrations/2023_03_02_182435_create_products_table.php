e197817ba0215fb9cc3d4dfe5c231d256d01ec12
Felipe1501 <felipebsantos1501@gmail.com>
2023-03-02 15:31:59 -0300

    introdução a migrations em laravel

--- curso-app/database/migrations/2023_03_02_182435_create_products_table.php
+++ curso-app/database/migrations/2023_03_02_182435_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('name', 150);
             $table->integer('qty');
             $table->text('description');
             $table->timestamps();
