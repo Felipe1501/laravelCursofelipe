ea2e6869b95a7b0f44e0c2077af93e14eeb97bc8
Felipe1501 <felipebsantos1501@gmail.com>
2023-03-04 23:56:03 -0300

    deletando dados

--- curso-app/app/Http/Controllers/EventController.php
+++ curso-app/app/Http/Controllers/EventController.php
@@ -87,6 +87,13 @@ class EventController extends Controller
 
     }
 
+    public function destroy($id){
+
+        Event::findOrFail($id)->delete();
+
+        return redirect('/dashboard')->with('msg', 'Evento excluído com sucesso!!!');
+    }
+
 }
 
 
